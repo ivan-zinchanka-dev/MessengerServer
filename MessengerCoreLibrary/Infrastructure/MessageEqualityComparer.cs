776f90ffa5262e533f3ff5beee4da2b7f2f2aad6
Ivan Zinchanka <75093640+ivan-zinchanka-dev@users.noreply.github.com>
2024-10-29 19:54:49 +0300

    feat: comparer

---
+++ MessengerCoreLibrary/Infrastructure/MessageEqualityComparer.cs
@@ -0,0 +1,28 @@
+﻿using MessengerCoreLibrary.Models;
+
+namespace MessengerCoreLibrary.Infrastructure;
+
+public class MessageEqualityComparer : IEqualityComparer<Message>
+{
+    public bool Equals(Message first, Message second)
+    {
+        if (ReferenceEquals(first, second)) 
+            return true;
+
+        if (first == null || second == null) 
+            return false;
+
+        return first.SenderNickname == second.SenderNickname
+               && first.ReceiverNickname == second.ReceiverNickname
+               && first.Text == second.Text
+               && first.PostDateTime == second.PostDateTime;
+    }
+
+    public int GetHashCode(Message message)
+    {
+        return message.SenderNickname.GetHashCode()
+               ^ message.ReceiverNickname.GetHashCode()
+               ^ message.Text.GetHashCode()
+               ^ message.PostDateTime.GetHashCode();
+    }
+}
